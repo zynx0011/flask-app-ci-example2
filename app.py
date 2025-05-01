41ae61fb168bceef2fb4723daf3a3028f1ac9f5d
zynx0011 <zrdrc31@gmail.com>
2025-05-01 12:22:00 +0000

    updated files

--- app.py
+++ app.py
@@ -16,3 +16,23 @@ def home():
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    
+    
+from flask import Flask,jsonify
+
+ 
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return jsonify(
+        status="up",
+        server="v2.0"
+    )
+
+@app.route("/")
+def home():
+    return "<b> Welcome to my project!! </b> <br/><br/> This is v2.0"
+   
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
